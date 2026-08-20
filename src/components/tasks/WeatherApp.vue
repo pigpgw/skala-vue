d37365e2a9d245eed34771c3cc1bb7e92fa6c01f
pigpgw <ceh2000212@gmail.com>
2026-08-20 10:18:09 +0900

    feat: [과제 1-4] 카드 선택과 상세보기 이벤트를 추가
    카드 클릭 시 선택 도시를 표시하고 @click.stop으로 상세보기 버튼의 이벤트 버블링을 차단했습니다.

--- src/components/tasks/WeatherApp.vue
+++ src/components/tasks/WeatherApp.vue
@@ -4,6 +4,9 @@ import { weatherData } from '@/data/weatherData'
 
 const weatherList = ref(weatherData)
 const searchQuery = ref('')
+const selectedCityInfo = ref('카드를 클릭하거나 검색해 보세요.')
+
+const showDetail = (cityName, status) => window.alert(`${cityName}의 현재 날씨는 [${status}] 상태입니다.`)
 </script>
 
 <template>
@@ -16,13 +19,25 @@ const searchQuery = ref('')
       <div>입력한 도시: {{ searchQuery }}</div>
     </div>
 
+    <div>
+      <div>선택 결과</div>
+      <div>{{ selectedCityInfo }}</div>
+    </div>
+
     <div class="weather-list">
-      <div v-for="item in weatherList" :key="item.id" class="weather-card">
+      <div
+        v-for="item in weatherList"
+        :key="item.id"
+        class="weather-card"
+        @click="selectedCityInfo = `${item.name}이 선택되었습니다.`"
+      >
         <div>{{ item.name }}</div>
         <div>현재 날씨: {{ item.status }}</div>
         <div>기온: {{ item.temp }}°C</div>
         <div v-if="item.temp >= 25">25도 이상으로 덥습니다.</div>
         <div v-else>25도 미만으로 선선합니다.</div>
+
+        <button @click.stop="showDetail(item.name, item.status)">날씨 자세히 보기</button>
       </div>
     </div>
   </main>
