3bed0ed09b6ecdd60c4413aac9f3c323e7d14b3e
pigpgw <ceh2000212@gmail.com>
2026-08-21 22:25:52 +0900

    feat: [과제 6-3] 지역 검색 결과 컴포넌트를 구성

--- src/components/weather/CitySearchPanel.vue
+++ src/components/weather/CitySearchPanel.vue
@@ -1,4 +1,5 @@
 <script setup>
+/** @typedef {import('@/types/region').Region} Region */
 import BaseInput from '@/components/common/BaseInput.vue'
 
 defineProps({
@@ -14,9 +15,13 @@ defineProps({
     type: String,
     required: true,
   },
+  regions: {
+    type: /** @type {import('vue').PropType<Region[]>}*/ (Array),
+    default: () => [],
+  },
 })
 
-const emit = defineEmits(['update-query'])
+const emit = defineEmits(['update-query', 'select-region'])
 
 /** @param {Event} event */
 const handleSearchInput = (event) => {
@@ -24,20 +29,23 @@ const handleSearchInput = (event) => {
   if (!(input instanceof HTMLInputElement)) return
   emit('update-query', input.value)
 }
+
+/** @param {Region} region */
+const selectRegion = (region) => {
+  emit('select-region', region)
+}
 </script>
 
 <template>
   <div class="city-search-panel">
     <div>검색할 도시</div>
     <div class="search-input-wrapper">
-      <BaseInput
-        variant="primary"
-        size="medium"
-        type="text"
-        :value="searchQuery"
-        placeholder="도시명을 입력하세요"
-        @input="handleSearchInput"
-      />
+      <BaseInput variant="primary" size="medium" type="text" :value="searchQuery" placeholder="도시명을 입력하세요" @input="handleSearchInput" />
+    </div>
+    <div v-if="regions.length > 0">
+      <button v-for="region in regions" :key="region.id" type="button" @click="selectRegion(region)">
+        {{ region.name }}
+      </button>
     </div>
     <div>입력한 도시: {{ searchQuery }}</div>
     <div>검색 결과 개수: {{ searchResultCount }}개</div>
