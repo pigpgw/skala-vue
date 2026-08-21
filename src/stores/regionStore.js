0d06dd0a5f510ba8604413c1a3482490a2272e7d
pigpgw <ceh2000212@gmail.com>
2026-08-21 22:25:38 +0900

    feat: [과제 6-3] 지역 Store를 구성

---
+++ src/stores/regionStore.js
@@ -0,0 +1,38 @@
+/** @typedef {import('@/types/region').RegionalCodeItem} RegionalCodeItem */
+/** @typedef {import('@/types/region').Region} Region */
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+import { getRegionalCodes } from '@/apis/regionalCode'
+
+export const useRegionStore = defineStore('region', () => {
+  /** @type {import('vue').Ref<Region[]>} */
+  const regions = ref([])
+
+  const fetchRegions = async () => {
+    if (regions.value.length > 0) return
+    const data = await getRegionalCodes()
+
+    /**@type {RegionalCodeItem[]} */
+    const items = data.Response.body.items.item
+
+    /** @type {Map<string, Region>} */
+    const regionMap = new Map()
+
+    items.forEach((region) => {
+      if (region.use_yn !== 'Y' || !region.sgg_cd || !region.sgg_nm) return
+      regionMap.set(region.sgg_cd, {
+        id: region.sgg_cd,
+        name: `${region.ctpv_nm} ${region.sgg_nm}`,
+        weatherName: region.sgg_nm,
+      })
+    })
+
+    regions.value = [...regionMap.values()]
+  }
+
+  return {
+    regions,
+    fetchRegions,
+  }
+})
